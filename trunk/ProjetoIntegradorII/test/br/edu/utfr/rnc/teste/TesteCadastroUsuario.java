e5f0648f6b8bb250564aa7d3c1cdafc987376079
unknown <Usuario@Usuario-PC.(none)>
2012-05-09 23:02:27 -0300

    Douglas, Marcelo: criado teste no Selenium para cadastro de usuarios

---
+++ trunk/ProjetoIntegradorII/test/br/edu/utfr/rnc/teste/TesteCadastroUsuario.java
@@ -0,0 +1,33 @@
+package br.edu.utfr.rnc.teste;
+
+import com.thoughtworks.selenium.*;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+public class TesteCadastroUsuario extends SeleneseTestCase {
+	@Before
+	public void setUp() throws Exception {
+		selenium = new DefaultSelenium("localhost", 4444, "*chrome", "http://localhost:8080/");
+		selenium.start();
+	}
+
+	@Test
+	public void testECadastroUsuario() throws Exception {
+		selenium.open("/ProjetoIntegradorII/");
+		selenium.click("css=#j_idt22 > span.ui-menuitem-text");
+		selenium.waitForPageToLoad("30000");
+		selenium.type("id=nome", "Marcelo");
+		selenium.type("id=login", "marcelo");
+		selenium.type("id=email", "marcelo@hotmail.com");
+		selenium.type("id=senha", "marcelo");
+		selenium.type("id=senha2", "marcelo");
+		selenium.click("id=save");
+		selenium.waitForPageToLoad("30000");
+	}
+
+	@After
+	public void tearDown() throws Exception {
+		selenium.stop();
+	}
+}
