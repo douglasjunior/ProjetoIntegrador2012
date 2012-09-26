b2ecb775fccd2a8ecbc070f04eaada9aad0c71dd
douglasjunior <nassifrroma@gmail.com>
2012-09-26 08:54:09 -0300

    Acertei o nome da persistencia utilizada pelo GenericDao, com o nome errado não é possível rodar o sistema.

--- trunk/ProjetoIntegradorII/src/java/br/edu/utfpr/rnc/dao/GenericDao.java
+++ trunk/ProjetoIntegradorII/src/java/br/edu/utfpr/rnc/dao/GenericDao.java
@@ -11,7 +11,7 @@ import javax.persistence.criteria.Root;
 @Stateless
 public class GenericDao {
 
-    @PersistenceContext(unitName = "pu")
+    @PersistenceContext(unitName = "ProjetoIntegradorIIPU")
     private EntityManager em;
 
     private EntityManager getEntityManager() {
