79fa6fdee16b1dec41a7109dfbe7c6c77f7f1e4d
marvastsi <marvas1987@gmail.com>
2012-11-06 21:27:22 -0200

    Marcelo, Douglas e Fabio: Foram adicionadas as tags shiro nas páginas.

--- trunk/ProjetoIntegradorII/src/java/br/edu/utfpr/rnc/autenticacao/RealmAutenticacao.java
+++ trunk/ProjetoIntegradorII/src/java/br/edu/utfpr/rnc/autenticacao/RealmAutenticacao.java
@@ -16,15 +16,21 @@ public class RealmAutenticacao extends JdbcRealm {
 
     private final String AUTENTICATION_QUERY = "SELECT senha FROM USUARIO WHERE login = ?";
     private final String USER_NAME_QUERY = "SELECT nome FROM USUARIO WHERE login = ?";
+    /**
+     * The default query used to retrieve the roles that apply to a user.
+     */
+    protected static final String USER_ROLES_QUERY = "SELECT nome FROM PAPEL WHERE id in("
+            + "SELECT papel_id FROM USUARIO WHERE login = ?)";
 
     public RealmAutenticacao() {
         super();
 
         authenticationQuery = AUTENTICATION_QUERY;
+        userRolesQuery = USER_ROLES_QUERY;
 
         InitialContext ic;
         DataSource ds;
- 
+
         try {
             ic = new InitialContext();
             ds = (DataSource) ic.lookup("aluno"); // jndi-name do arquivo glassfish-resources.xml
