aa35b30095f2b6fde9abe4a1471a8a9323575c77
douglasjunior <nassifrroma@gmail.com>
2012-11-06 22:23:32 -0200

    douglas, maiara: criado cliente web para consumir webservice. douglas: adicionado toString nos objetos Usuario e Departamento no projeto principal.

--- trunk/ClienteWebWS/src/java/RncBean.java
+++ trunk/ClienteWebWS/src/java/RncBean.java
@@ -5,17 +5,21 @@
 
 import br.edu.utfpr.rnc.webservice.ListarRNC_Service;
 import br.edu.utfpr.rnc.webservice.RncService;
+import java.util.ArrayList;
 import java.util.List;
 import javax.faces.bean.ManagedBean;
-import javax.faces.bean.RequestScoped;
+import javax.faces.bean.SessionScoped;
 
 /**
  *
  * @author Douglas
  */
 @ManagedBean
-@RequestScoped
+@SessionScoped
 public class RncBean {
+
+    private List<RncService> rncs;
+    private String situacao;
 
     /**
      * Creates a new instance of RncBean
@@ -24,13 +28,23 @@ public class RncBean {
     }
 
     public List<RncService> getRncs() {
-
-        ListarRNC_Service service = new ListarRNC_Service();
-
-        List<RncService> rncs = service.getListarRNCPort().listarRncs("");
-
-        System.err.println("Listando: " + rncs);
-
         return rncs;
+    }
+
+    public void buscar() {
+        if (situacao == null || situacao.isEmpty()) {
+            rncs = new ArrayList<RncService>();
+            return;
+        }
+        ListarRNC_Service service = new ListarRNC_Service();
+        rncs = service.getListarRNCPort().listarRncs(situacao);
+    }
+
+    public String getSituacao() {
+        return situacao;
+    }
+
+    public void setSituacao(String situacao) {
+        this.situacao = situacao;
     }
 }
